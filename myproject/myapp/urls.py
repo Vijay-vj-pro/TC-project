753eede4148bcfb2dcef567c292482cb0f693d7e
Jagan-hw <jagannathan.m.2004@gmail.com>
2024-09-10 10:58:23 +0530

    name changed

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.imageUpload, name='imageUpload'),
-    path('search/', views.search_view, name='search_view'),
-    path('image/<int:id>/', views.image_detail, name='image_detail'),
+    path('',views.imageUpload,name='imageUpload'),
+    path('search/',views.search_view,name='search_view'),
+    path('image/<int:id>/',views.image_detail,name='image_detail'),
 ]
